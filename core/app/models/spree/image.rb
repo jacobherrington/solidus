1d365c22b22ec8c0e55eb6a7b8bc9e74fd38094e
fastjames <fastjames@gmail.com>
2018-08-24 13:51:41 -0500

    Revert our styles extension point
    This breaks parts of the admin.

--- core/app/models/spree/image.rb
+++ core/app/models/spree/image.rb
@@ -5,7 +5,7 @@ module Spree
     validate :no_attachment_errors
 
     has_attached_file :attachment,
-                      :styles => lambda { |a| a.instance.attachment_styles },
+                      styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' },
                       default_style: :product,
                       default_url: 'noimage/:style.png',
                       url: :image_asset_url,
@@ -49,15 +49,6 @@ module Spree
 
     def image_asset_path
       ':rails_root/public/spree/products/:id/:style/:basename.:extension'
-    end
-
-    def attachment_styles
-      {
-        mini: '48x48>',
-        small: '100x100>',
-        product: '240x240>',
-        large: '600x600>'
-      }
     end
 
     private
