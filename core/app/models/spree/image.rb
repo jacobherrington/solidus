d45534f426bda304fa427e71eec240d475a4cc4c
sdbradley <scott.bradley@daveramsey.com>
2018-11-06 00:18:17 -0600

    [DAM-005] validate only if not saving digital asset to image

--- core/app/models/spree/image.rb
+++ core/app/models/spree/image.rb
@@ -2,7 +2,8 @@
 
 module Spree
   class Image < Asset
-    validate :no_attachment_errors
+
+    validate :no_attachment_errors, unless: lambda { :digital_asset_id.present? }
 
     has_attached_file :attachment,
                       styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' },
@@ -13,11 +14,11 @@ module Spree
                       convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
     validates_attachment :attachment,
       presence: true,
-      content_type: { content_type: %w(image/jpeg image/jpg image/png image/gif) }
+      content_type: { content_type: %w(image/jpeg image/jpg image/png image/gif) }, unless: lambda { :digital_asset_id.present? }
 
     # save the w,h of the original image (from which others can be calculated)
     # we need to look at the write-queue for images which have not been saved yet
-    after_post_process :find_dimensions, if: :attachment_accepted?
+    after_post_process :find_dimensions, unless: lambda { :digital_asset_id.present? }
 
     # used by admin products autocomplete
     def mini_url
@@ -25,6 +26,7 @@ module Spree
     end
 
     def find_dimensions
+      return unless :attachment_accepted?
       temporary = attachment.queued_for_write[:original]
       filename = temporary.path unless temporary.nil?
       filename = attachment.path if filename.blank?
